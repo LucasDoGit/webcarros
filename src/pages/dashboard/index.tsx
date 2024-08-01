673cebc9b3c52d086c6ae1587e67aac67c5d4d85
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-08-01 17:58:47 -0300

    feat: visualização e exclusão de carros
    o usuário consegue visualizar os carros publicados e também excluir os anúncios.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,8 +3,9 @@ import { Container } from "../../components/container";
 import { DashboradHeader } from '../../components/panelheader'
 import { FiTrash2 } from "react-icons/fi";
 
-import { getDocs, collection, query, where } from "firebase/firestore";
-import { db } from "../../services/firebaseConnection";
+import { getDocs, collection, query, where, doc, deleteDoc } from "firebase/firestore";
+import { db, storage } from "../../services/firebaseConnection";
+import { ref, deleteObject} from 'firebase/storage'
 import { AuthContext } from "../../contexts/AuthContext";
 
 interface CarProps {
@@ -26,13 +27,12 @@ interface CarImageProps {
 
 export function Dashboard() {
     const [cars, setCars] = useState<CarProps[]>([])
-    const { user } = useContext(AuthContext)
-    const [loadImages, setLoadImages] = useState<string[]>([])
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
 
         function loadCars() {
-            if(!user?.uid){
+            if (!user?.uid) {
                 return;
             }
             const carsRef = collection(db, "cars")
@@ -65,44 +65,66 @@ export function Dashboard() {
         loadCars();
     })
 
+    async function handleDeleteCar(car: CarProps){
+        const itemCar = car;
+        
+        const docRef = doc(db, "cars", itemCar.id)
+        await deleteDoc(docRef);
+
+        itemCar.images.map( async(image) => {
+            const imagePath = `images/${itemCar.uid}/${image.name}`;
+            const imageRef = ref(storage, imagePath)
+
+            try {
+                await deleteObject(imageRef)
+                setCars(cars.filter(car => car.id !== itemCar.id))
+            } catch(err) {
+                console.log("Erro ao excluir a foto", err)
+            }
+        })
+        
+    }
+
     return (
         <Container>
-            <DashboradHeader/>
+            <DashboradHeader />
 
-        <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-            <section className="w-full bg-white rounded-lg relative">
-                <button 
-                className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
-                onClick={ () => {}}
-                >
-                    <FiTrash2 size={26} color="#000"/>
-                </button>
-                <img 
-                    className="w-full rounded-lg mb-2 max-h-70" 
-                    alt="Carro"
-                    src="https://firebasestorage.googleapis.com/v0/b/webcarros-d1d40.appspot.com/o/images%2Fm0tQppbWMTXFAw5sgB4ZcDIu1B42%2F071e526d-900a-4f3d-b6d8-5825eb9d0832?alt=media&token=7220d286-569e-402c-8450-873303fd9f16" 
-                />
-                <p className="font-bold mt-1 px-2 mb-2">Nissa Versa</p>
+                {cars.map(car => (
+                    <section key={car.id} className="w-full bg-white rounded-lg relative">
+                        <button
+                            className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
+                            onClick={ () => handleDeleteCar(car) }
+                        >
+                            <FiTrash2 size={26} color="#000" />
+                        </button>
+                        <img
+                            className="w-full rounded-lg mb-2 max-h-70"
+                            alt={car.name}
+                            src={car.images[0].url}
+                        />
+                        <p className="font-bold mt-1 px-2 mb-2">{car.name}</p>
 
-                <div className="flex flex-col px-2">
-                    <span className="flex flex-col px-2"> 
-                        Ano 2016/2016 | 230.000 km
-                    </span>
-                    <strong className="text-black font-bold mt-4">
-                        R$ 150.000
-                    </strong>
-                </div>
+                        <div className="flex flex-col px-2">
+                            <span className="flex flex-col px-2">
+                                Ano {car.year} | {car.km} km
+                            </span>
+                            <strong className="text-black font-bold mt-4">
+                                R$ {car.price}
+                            </strong>
+                        </div>
 
-                <div className="w-full h-px bg-slate-200 my-2"></div>
-                <div className="px-2 pb-2">
-                    <span className="text-black">
-                        Campo Grande - MS
-                    </span>
-                </div>
+                        <div className="w-full h-px bg-slate-200 my-2"></div>
+                        <div className="px-2 pb-2">
+                            <span className="text-black">
+                                {car.city}
+                            </span>
+                        </div>
 
-            </section>
-        </main>
+                    </section>
+                ))}
+            </main>
 
         </Container>
     )
