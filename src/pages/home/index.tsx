ae7afc99427eabb28786458b881370b5565697db
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2024-08-05 23:52:14 -0300

    feat(home): função de pesquisa de veículos
    ajustado o cadastrado do nome dos veículos para uppercase e tornar a pesquisa funcional na página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
 
 import { Link } from "react-router-dom";
-import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { collection, query, getDocs, orderBy, where } from "firebase/firestore";
 import { db } from "../../services/firebaseConnection";
 
 interface CarsProps {
@@ -25,41 +25,78 @@ interface CarImageProps {
 export function Home() {
     const [cars, setCars] = useState<CarsProps[]>([])
     const [loadImages, setLoadImages] = useState<string[]>([])
+    const [input, setInput] = useState("")
 
     useEffect(() => {
-        function loadCars() {
-            const carsRef = collection(db, "cars")
-            const queryRef = query(carsRef, orderBy("created", "desc"))
 
-            getDocs(queryRef)
-                .then((snapshot) => {
-                    let listcars = [] as CarsProps[];
-
-                    snapshot.forEach(doc => {
-                        listcars.push({
-                            id: doc.id,
-                            name: doc.data().name,
-                            year: doc.data().year,
-                            km: doc.data().km,
-                            city: doc.data().city,
-                            price: doc.data().price,
-                            images: doc.data().images,
-                            uid: doc.data().uid
-                        })
-                    })
-
-                    setCars(listcars)
-                })
-                .catch((err) => {
-                    console.log("Erro ao buscar carros", err)
-                })
-        }
 
         loadCars();
-    })
+    }, [])
 
-    function handleImageLoad(id: string){
+    function loadCars() {
+        const carsRef = collection(db, "cars")
+        const queryRef = query(carsRef, orderBy("created", "desc"))
+
+        getDocs(queryRef)
+            .then((snapshot) => {
+                let listcars = [] as CarsProps[];
+
+                snapshot.forEach(doc => {
+                    listcars.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        year: doc.data().year,
+                        km: doc.data().km,
+                        city: doc.data().city,
+                        price: doc.data().price,
+                        images: doc.data().images,
+                        uid: doc.data().uid
+                    })
+                })
+
+                setCars(listcars)
+            })
+            .catch((err) => {
+                console.log("Erro ao buscar carros", err)
+            })
+    }
+
+    function handleImageLoad(id: string) {
         setLoadImages((prevImageLoaded) => [...prevImageLoaded, id])
+    }
+
+    async function handleSearchCar() {
+        if (input === "") {
+            loadCars();
+            return;
+        }
+
+        setCars([]);
+        setLoadImages([]);
+
+        const q = query(collection(db, "cars"),
+            where("name", ">=", input.toUpperCase()),
+            where("name", "<=", input.toUpperCase() + "\uf8ff")
+        )
+
+        const querySnapshot = await getDocs(q)
+
+        let listcars = [] as CarsProps[];
+
+        querySnapshot.forEach((doc) => {
+            listcars.push({
+                id: doc.id,
+                name: doc.data().name,
+                year: doc.data().year,
+                km: doc.data().km,
+                city: doc.data().city,
+                price: doc.data().price,
+                images: doc.data().images,
+                uid: doc.data().uid
+            })
+        })
+
+        setCars(listcars)
     }
 
     return (
@@ -69,9 +106,12 @@ export function Home() {
                 <input
                     className="w-full border-2 rounded-lg h-9 px-3 outline-none"
                     placeholder="Digite o nome do carro"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
                 />
                 <button
                     className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+                    onClick={handleSearchCar}
                 >
                     Buscar
                 </button>
@@ -86,16 +126,16 @@ export function Home() {
                 {cars.map(car => (
                     <Link key={car.id} to={`/car/${car.id}`}>
                         <section className="w-full bg-white rounded-lg">
-                            <div 
+                            <div
                                 className="w-full h-72 rounded-lg bg-slate-200"
-                                style={{ display: loadImages.includes(car.id) ? "none" : "block"}}
+                                style={{ display: loadImages.includes(car.id) ? "none" : "block" }}
                             ></div>
                             <img
                                 className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
                                 src={car.images[0].url}
                                 alt={car.name}
-                                onLoad={ () => handleImageLoad(car.id)}
-                                style={{ display: loadImages.includes(car.id) ? "block" : "none"}}
+                                onLoad={() => handleImageLoad(car.id)}
+                                style={{ display: loadImages.includes(car.id) ? "block" : "none" }}
                             />
 
                             <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
